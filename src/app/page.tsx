d374e825cf3d5d26e8367481028db426aa7c394d
Jirka Manak <41122948+manakjiri@users.noreply.github.com>
2024-11-05 13:45:01 +0100

    add instructions to about page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -30,11 +30,15 @@ export default function Home() {
     <main>
       <Container maxWidth="xl">
         <Typography
-            variant="h2"
+            variant="h4"
             sx={{ mt: 4, mb: 4, textAlign: "center" }}  
             >
-            Welcome to the about page
+            Welcome to quick-flash!
         </Typography>
+        <Typography>1.Quick-flash needs access to storage hosting the firmware binaries, set it up according to the <a>tutorial</a> first.</Typography>
+        <Typography>2.Once setup, come back here and navigate to Flash -> Storage, where you can add it.</Typography>
+        <Typography>3.Now you should be able to browse the storage you just added (Hint: you can double-click instead of clicking Continue).</Typography>
+        <Typography>4.Connect your target and flash it!</Typography>
       </Container>
     </main>
   );
