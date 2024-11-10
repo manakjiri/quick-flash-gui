292707022614e8c581864f09f4241bce240d7ce1
Jirka Manak <41122948+manakjiri@users.noreply.github.com>
2024-11-10 15:33:24 +0100

    fix about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,9 @@
 "use client";
 
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "../styles/Home.module.css";
-import { Alert, Box, Button, Container, Link, Snackbar } from "@mui/material";
-import NextLink from "next/link";
-const inter = Inter({ subsets: ["latin"] });
-
-import * as React from "react";
+import {Container } from "@mui/material";
 import Typography from "@mui/material/Typography";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import Paper from "@mui/material/Paper";
-import { useRouter } from "next/router";
-import { join } from "path";
-import { useState } from "react";
-import StorageTable from "@/components/StorageTable";
 
 export default function Home() {
-  const [loading, setLoading] = useState(false);
-
-  const rows: any[] = [{ id: "1" }];
-  const handleStart = () => {
-    console.log("Starting something");
-  };
   return (
     <main>
       <Container maxWidth="xl">
@@ -36,7 +14,7 @@ export default function Home() {
             Welcome to quick-flash!
         </Typography>
         <Typography>1.Quick-flash needs access to storage hosting the firmware binaries, set it up according to the <a>tutorial</a> first.</Typography>
-        <Typography>2.Once setup, come back here and navigate to Flash -> Storage, where you can add it.</Typography>
+        <Typography>2.Once setup, come back here and navigate to Flash &rarr; Storage, where you can add it.</Typography>
         <Typography>3.Now you should be able to browse the storage you just added (Hint: you can double-click instead of clicking Continue).</Typography>
         <Typography>4.Connect your target and flash it!</Typography>
       </Container>
